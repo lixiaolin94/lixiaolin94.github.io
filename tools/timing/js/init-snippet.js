52ff6abed2608dccace14c6fd29c82b58df0d430
李笑林 <niloa.i@outlook.com>
2024-11-24 15:17:49 +0800

    Refactor CSS and HTML structure for improved styling and consistency; update JavaScript snippet initialization

--- tools/timing/js/init-snippet.js
+++ tools/timing/js/init-snippet.js
@@ -5,13 +5,9 @@ document.getElementById("spring-output-android-springanimation").innerHTML = `va
 
 document.getElementById("spring-output-ios-spring-duration-bounce").innerHTML = `let animation = Animation.spring(duration: <span class="output-args output-response"></span>, bounce: <span class="output-args output-bounce"></span>)`;
 
-document.getElementById(
-  "spring-output-ios-spring-response-dampingRatio"
-).innerHTML = `let animation = Animation.spring(response: <span class="output-args output-response"></span>, dampingRatio: <span class="output-args output-dampingRatio"></span>);`;
+document.getElementById("spring-output-ios-spring-response-dampingRatio").innerHTML = `let animation = Animation.spring(response: <span class="output-args output-response"></span>, dampingRatio: <span class="output-args output-dampingRatio"></span>);`;
 
-document.getElementById(
-  "spring-output-ios-spring-mass-stiffness-damping"
-).innerHTML = `let animation = Animation.spring(mass: <span class="output-args output-mass"></span>, stiffness: <span class="output-args output-stiffness"></span>, damping: <span class="output-args output-damping"></span>);`;
+document.getElementById("spring-output-ios-spring-mass-stiffness-damping").innerHTML = `let animation = Animation.spring(mass: <span class="output-args output-mass"></span>, stiffness: <span class="output-args output-stiffness"></span>, damping: <span class="output-args output-damping"></span>);`;
 
 document.getElementById("spring-output-ios-caspringanimation").innerHTML = `let springAnimation = CASpringAnimation(keyPath: "transform.translation.x")
 springAnimation.mass = <span class="output-args output-mass"></span>
@@ -22,6 +18,18 @@ springAnimation.toValue = 100
 springAnimation.duration = springAnimation.settlingDuration
 springAnimation.isRemovedOnCompletion = false
 springAnimation.fillMode = .forwards`;
+
+document.getElementById("spring-output-web-css").innerHTML = `@keyframes spring-animation-keyframes {
+  from { transform: translateX(0px) }
+  to { transform: translateX(100px) }
+}
+
+.spring-animation {
+  animation-name: spring-animation-keyframes;
+  animation-duration: <span class="output-args"><span class="output-response"></span>s</span>;
+  animation-timing-function: linear(0 0%, 0.5 50%, 1 100%);
+  animation-fill-mode: forwards;
+}`;
 
 document.getElementById("spring-output-web-framer-time").innerHTML = `import { motion } from "framer-motion"
 
